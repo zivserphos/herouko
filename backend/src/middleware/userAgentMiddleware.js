6598ef1fc77d9ce93c47ad723ab669b60deb2cd2
zivserphos <zivfromisrael@gmail.com>
2021-11-01 15:48:39 +0200

    development

--- backend/src/middleware/userAgentMiddleware.js
+++ backend/src/middleware/userAgentMiddleware.js
@@ -5,6 +5,7 @@ const userAgentMiddleware = function(req, res, next) {
   req.uaData = uaParser(req.headers['user-agent']) || {};
   req.uaData.ip = req.socket.remoteAddress;
   next();
+  
 }
 
 module.exports = userAgentMiddleware;
